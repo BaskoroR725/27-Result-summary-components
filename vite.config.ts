46a0bd730df753288845f455857d4e34f2251278
Baskoro Ramadhan <baskoror725@gmail.com>
2026-02-12 22:57:21 +0800

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/27-Result-summary-components/",
   plugins: [react(), tailwindcss()],
 })
